c41631d4dae1424df253a3755389e240036c964f
FaithKwamboka <okongofaith3@gmail.com>
2022-03-19 01:07:31 +0300

    Added effects for order events

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("table").hide();
+    $(".add-btn").hide();
+    $(".add-info").hide();
+    $(".btn.yes").hide();
+    $(".btn.no").hide();
+    $(".add-info h5").hide();
+    
+}
