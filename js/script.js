11b5f048a6f64f790c9b4dd359ee04a5703be7fc
FaithKwamboka <okongofaith3@gmail.com>
2022-03-19 01:14:02 +0300

    Added effects for order events

--- js/script.js
+++ js/script.js
@@ -5,5 +5,22 @@ $(document).ready(function(){
     $(".btn.yes").hide();
     $(".btn.no").hide();
     $(".add-info h5").hide();
+
+    $('.btn.order').click(function() {
+        var pizzaSize  = $(".size option:selected").val();
+        var pizzaToppings = $(".toppings option:selected").val();
+        var pizzaCrust= $(".crust option:selected").val();
+        var total = parseInt(pizzaSize ) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+        var order = 1;
+        var grandTotal = 0;
     
+        $("table").show();
+        $(".add-btn").show();
+        $(".btn.order").hide();
+    
+        $("#size").html($(".size option:selected").text() + " - " + pizzaSize);
+        $("#toppings").html($(".toppings option:selected").text() + " - " + pizzaToppings);
+        $("#crust").html($(".crust option:selected").text() + " - " + pizzaCrust);
+        $("#total").html(total);
+        
 }
